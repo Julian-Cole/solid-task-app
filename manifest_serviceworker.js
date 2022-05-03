6d2a52f32fd83bfd5c96ce95c4790aec209f8f2e
Jules Cole <49248917+Julian-Cole@users.noreply.github.com>
2022-05-03 23:56:25 +0100

    Add files via upload

--- manifest_serviceworker.js
+++ manifest_serviceworker.js
@@ -12,15 +12,14 @@ self.addEventListener('install', async event=>{
     const cache = await caches.open('static-cache');
     cache.addAll(staticAssets);
 });
-
-
 self.addEventListener('fetch', event => {
-    const req = event.request;
-    const url = new URL(req.url);
-    event.respondWith(cache(req));
+  // Prevent the default, and handle the request ourselves.
+  event.respondWith(async function() {
+    // Try to get the response from a cache.
+    const cachedResponse = await caches.match(event.request);
+    // Return it if we found one.
+    if (cachedResponse) return cachedResponse;
+    // If we didn't find a match in the cache, use the network.
+    return fetch(event.request);
+  }());
 });
-
-async function cache(req){
-    const cachedResponse = caches.match(req);
-    return cachedResponse || fetch(req);
-}
